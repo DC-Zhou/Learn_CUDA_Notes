06208bff7bf4c3b3f4ad54194eabcdfd9f98eadb
Zhouzhou <1197236910@qq.com>
2023-07-31 14:48:59 +0800

    add: chapter-01
    Signed-off-by: Zhouzhou <1197236910@qq.com>

---
+++ README.md
@@ -0,0 +1,22 @@
+# Use NSYS
+
+'''
+make all
+nsys profile --stats=true ./sgemm
+'''
+
+# CUDA Beginner guide
+
+## About CUDA programming
+
+
+## Memory management in CUDA 
+
+
+## CUDA thread programming
+
+
+## Debug and Profile in CUDA 
+
+
+## 
---
+++ hello_world/hello_world.cu
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void print_from_gpu(void) {
+    printf("Hello World! From thread [%d, %d]  from devices \n", threadIdx.x, blockIdx.x);
+}
+
+int main(void) {
+    printf("Hello World from Host! \n");
+    print_from_gpu<<<4,2>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
